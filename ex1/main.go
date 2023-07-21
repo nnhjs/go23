adf7915c5480fdf3cd9fd5959bb992675d42c1f2
nnhjs <nnhungjs@gmail.com>
2023-07-21 11:53:02 +0700

    workaround ex1

--- ex1/main.go
+++ ex1/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Enter Your First Name: ")
+	var first string
+	fmt.Scanln(&first)
+
+	fmt.Println("Enter Your Middle Name: ")
+	var middle string
+	fmt.Scanln(&middle)
+
+	fmt.Println("Enter Your Last Name: ")
+	var last string
+	fmt.Scanln(&last)
+
+	fmt.Println("Enter Your Country Code ")
+	var country string
+	fmt.Scanln(&country)
+
+	if country != "VN" {
+		tmp := first
+		first = last
+		last = tmp
+	}
+
+	if middle != "" {
+		fmt.Println(first + " " + middle + " " + last)
+	} else {
+		fmt.Println(first + " " + last)
+	}
+
 }
